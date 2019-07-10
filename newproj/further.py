1a85f2acbbcf92f95888f197995ae778dd35fb9b
piyush-98 <arjunguptaa98@gmail.com>
2019-07-10 14:13:47 +0530

    adding templates

--- newproj/further.py
+++ newproj/further.py
@@ -13,7 +13,12 @@ def index():
 
 @app.route('/hello/<user>')
 def hello_name(user):
-   return render_template('hello.html', name = user)
+   return render_template('hello.html', name = user) ## This Renders the html page with name as the placeholder
+
+@app.route('/result')
+def result():
+   dict = {'phy':50,'che':60,'maths':70}
+   return render_template('hello.html', result = dict)
 
 if __name__ == '__main__':
    app.run(debug = True)
